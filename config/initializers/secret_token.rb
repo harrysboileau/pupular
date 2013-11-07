ffcc23f416557e6205f0fbb0da06ee86a54d6993
Joe Levering <joseph.w.levering@gmail.com>
2013-11-06 18:00:42 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pupular::Application.config.secret_token = '65143bc042160580050c4a7baf043c815961f0eeb366363a54f875626ca91f4878d4dff22498c10d2c60565a39e8c20a6d14031ef9e47fa6bcf49aae90f643cd'
