472fec138bc8fa7b6ea43e9696366ce2b0661ec2
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-08 12:35:39 -0600

    added accept_pal and deny_pal methods in the Dog controller, added specs to test methods

--- spec/models/dog_spec.rb
+++ spec/models/dog_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe Dog do
-
   context "associations" do
     it { should have_many(:friendships) }
     it { should have_many(:pals)}
@@ -13,9 +12,9 @@ describe Dog do
     it { should have_many(:received_messages)}
     it { should have_many(:events)}
     it { should have_many(:attended_events)}
-end
+  end
 
-context "validations" do
+  context "validations" do
     it { should validate_presence_of(:email)}
     it { should validate_uniqueness_of(:email) }
     it { should_not allow_value("zack").for(:email) }
@@ -25,5 +24,49 @@ context "validations" do
     it { should_not allow_value("passw").for(:password)}
     it { should_not allow_value("passwordpassw").for(:password)}
     it { should validate_presence_of(:name)}
+  end
+
+  context "methods" do
+    describe "#accept_pal" do
+    let(:dog) { Dog.create(attributes_for(:dog)) }
+    let(:pal) { Dog.create(attributes_for(:dog)) }
+    before(:each) do
+      @dog = dog
+      @pal = pal
+      @dog.pending_pals << @pal
+      @dog.accept_pal(@pal.id)
+    end
+
+    it "will add pending_pal to pals" do
+      dog = Dog.find(@dog.id)
+      expect(dog.pals).to include(@pal)
+    end
+
+    it "will remove pending_pal from pending_pals" do
+      dog = Dog.find(@dog.id)
+      expect(dog.pending_pals).to_not include(@pal)
+    end
+  end
+  describe "#deny_pal" do
+    let(:dog) { Dog.create(attributes_for(:dog)) }
+    let(:pal) { Dog.create(attributes_for(:dog)) }
+    before(:each) do
+      @dog = dog
+      @pal = pal
+      @dog.pending_pals << @pal
+      @pending_friendship = @dog.pending_friendships.find_by_pending_friend_id(@pal.id)
+      @dog.deny_pal(@pal.id)
+    end
+
+    it "will remove pending_pal from pending_pals" do
+      dog = Dog.find(@dog.id)
+      expect(dog.pending_pals).to_not include(@pal)
+    end
+
+    it "will destroy the pending_friendship" do
+      dog = Dog.find(@dog.id)
+      expect(dog.pending_friendships).to_not include(@pending_friendship)
+    end
+  end
 end
 end
