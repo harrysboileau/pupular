165251a6b600b969e05bc631824e6b4493551c2c
zachmokahn <zachmokahn@gmail.com>
2013-11-08 06:44:46 -0600

    added message model, associations, validations, and spec

---
+++ app/models/messages.rb
@@ -0,0 +1,15 @@
+class Message < ActiveRecord::Base
+  belongs_to :sender, class_name: "Dog"
+  has_many :inbox_messages
+  has_many :recipients, through: :inbox_messages
+  attr_accessible :content, :subject, :type
+
+  validates_presence_of :content, :subject, :sender_id, :type
+  validates :type, inclusion: { in: ["Automated", "Personal"], message: "%{value} is not a valid message type" }
+end
+
+class Automated < Message
+end
+
+class Personal < Message
+end
