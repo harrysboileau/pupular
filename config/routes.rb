cc0703984c1d36407722d5a3456dbd1994b7f604
zachmokahn <zachmokahn@gmail.com>
2013-11-08 06:48:04 -0600

    added initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,26 @@
 Pupular::Application.routes.draw do
-  
+
   resources :dogs do
-    resources :events 
+    resources :events
     resources :messages
-    resources :profiles, :only => [:new, :edit]
-    get '/name' => 'dogs#name'
   end
 
+  resources :profiles, :only => [:new, :create, :edit]
+
+
+  post '/name' => 'dogs#savehuman'
+  get '/name' => 'dogs#name'
 
   get '/doghouse' => 'dogs#doghouse'
+
   get '/search' => 'dogs#search'
   post '/search' => 'dogs#filter_search'
   post '/add_friend/:pending_pal_id' => 'dogs#add_friend'
+
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
   post '/signout' => 'sessions#signout'
   root :to => 'welcome#index'
-
-
 
 end
 
