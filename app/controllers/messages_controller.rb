5398df79ce98246a98f4aa2d3801275ba8b83962
Apprentice <apprentice@dbc-chicago-001.local>
2013-11-08 17:13:08 -0600

    added message inbox and new message views, messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,20 @@
 class MessagesController < ApplicationController
   def index
+    @dog = Dog.find(1)
+    @inbox = @dog.received_messages
   end
+
+  def new
+    
+  end
+
+  def create
+
+    p params
+    # dog2.received_messages << dog.sent_messages.create(type:"Automated",subject:"get with it",content:"get at it")
+    # redirect_to doghouse_path(1)
+  end
+
+
 
 end
