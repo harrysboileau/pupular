d432b1ce29b055ef331c7cb6b420816ad3d57293
zachmokahn <zachmokahn@gmail.com>
2013-11-08 06:46:11 -0600

    added EventAttendance model, associations, validations, and spec

---
+++ spec/models/event_attendance_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe EventAttendance do
+  context "associations" do
+    it { should belong_to(:attendee) }
+    it { should belong_to(:attended_events) }
+  end
+
+  context "validations" do
+    it { should validate_presence_of(:dog_id)}
+    it { should validate_presence_of(:event_id)}
+    it { should validate_uniqueness_of(:event_id).scoped_to(:dog_id) }
+  end
+end
