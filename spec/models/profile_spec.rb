edd48de7771cccbc156c6b97f87f5f54ab1cc132
Joe Levering <joseph.w.levering@gmail.com>
2013-11-09 13:53:25 -0600

    Cleaned up sign in/signup processes. Added logic to display help when dog house is empty.

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -7,14 +7,7 @@ describe Profile do
   end
 
   context "validations" do
-    it { should validate_presence_of(:age)}
-    it { should validate_presence_of(:breed)}
-    it { should validate_presence_of(:dog_id)}
-    it { should validate_presence_of(:gender)}
-    it { should ensure_inclusion_of(:gender).in_array(["male","female"]) }
-    it { should validate_presence_of(:location)}
-    it { should validate_presence_of(:spayed)}
-    it { should validate_presence_of(:size)}
-    it { should ensure_inclusion_of(:size).in_array(["toy", "small", "medium", "large", "extra-large"]) }
+    it { should ensure_inclusion_of(:gender).in_array(["", "Male","Female"]) }
+    it { should ensure_inclusion_of(:size).in_array(["", "Toy", "Small", "Medium", "Large", "Extra-large"]) }
   end
 end
