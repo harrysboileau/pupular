6bf91ee1a0ffc5488934f90f48fee9a429dd1922
Joe Levering <joseph.w.levering@gmail.com>
2013-11-08 19:31:44 -0600

    Added sign in/sign out (sessions)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,28 @@
 class SessionsController < ApplicationController
   def new
+    @dog_session = DogSession.new
   end
 
   def create
-    # Authenticate
+    @dog_session = DogSession.new(params[:dog_session])
+
     # if incomplete_user? redirect_to name; else
-    redirect_to "/doghouse"
+
+    if @dog_session.save
+      if current_dog.is_registered?
+        redirect_to "/doghouse"
+      else
+        redirect_to '/name'
+      end
+    else
+      render "new"
+    end
   end
 
-  def signout
+  def destroy
+    @dog_session = DogSession.find
+    @dog_session.destroy
 
+    redirect_to :root
   end
 end
---
+++ app/models/dog_session.rb
@@ -0,0 +1,3 @@
+class DogSession < Authlogic::Session::Base
+  find_by_login_method :find_by_username_or_email
+end
