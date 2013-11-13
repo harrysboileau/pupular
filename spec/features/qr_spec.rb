3cafa849ce5555752e62b6c104b5aed0a6aea5ed
zachmokahn <zachmokahn@gmail.com>
2013-11-13 15:10:08 -0600

    added pending integration tests for the QR codes, included all support modules in the spec_helper

---
+++ spec/features/qr_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+feature "QR" do
+  scenario "QR code will appear on the navbar"
+  scenario "A larger QR code will appear on the screen when clicked"
+  scenario "The Larger QR code will disappear after the QR code is clicked anywhere"
+  scenario "The Larger QR code will disappear after the screen is clicked anywhere"
+end
---
+++ spec/support/qr_spec_helper.rb
@@ -0,0 +1,2 @@
+module QRSpecHelper
+end
