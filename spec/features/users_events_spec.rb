a71e9357a3bfbb0c7b1a6ec09a558ee8821fa8ae
zachmokahn <zachmokahn@gmail.com>
2013-11-13 15:09:01 -0600

    added integration tests for the events pages

---
+++ spec/features/users_events_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+feature 'UsersEventsSpecHelper' do
+  scenario "Dog can go to the event page from the doghouse"
+  scenario "Event page will display event information"
+  scenario "Dog can add friends from the event page"
+  scenario "Dog can edit the Event"
+end
