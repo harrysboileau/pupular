daa204a5ef21dd144a7933d795ac5821ff094e04
zachmokahn <zachmokahn@gmail.com>
2013-11-13 15:07:54 -0600

    added integration tests for profile pages

---
+++ spec/features/other_dog_profile_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+feature "OtherDogProfile" do
+  scenario "Other Dogs Profile can be is accessed from search bar"
+  scenario "Other Dogs Profile displays an add button if they are not friends"
+  scenario "Other Dogs Profile does not display an add button if they are friends"
+  scenario "Other Dogs Profile displays profile infomration if they have filled it out"
+  scenario "Other Dogs Prfoile displays '###Message###' if they have not filled out their profile"
+end
