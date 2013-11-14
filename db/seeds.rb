7791f3eb5ffbb1e1f3160ce6ab9413350eaf0b8d
zachmokahn <zachmokahn@gmail.com>
2013-11-14 17:55:42 -0600

    created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,27 @@
 
 require 'faker'
 
+def random_image
+  File.open(random_image_url)
+end
+
+def random_image_url
+    File.join(Rails.root.join('app', 'assets', 'images'), "#{random_number}.jpg")
+end
+
+def random_number
+    numbers = (1..10).to_a
+    numbers.map! { |x| x.to_s }
+    numbers.sample
+end
+
 100.times do
 	username = Faker::Internet.user_name
 	password = "password"
 	name = Faker::Name.name
 	email = Faker::Internet.email
 	dog = Dog.create(username: username, email: email, password: password, name: name )
-  profile = Profile.new(breed:"Mutt", location:"Chicago", age:8, size:"Medium", gender:"Male", spayed:true)
-  profile.image.store!(File.open(File.join(Rails.root.join('app', 'assets', 'images'), 'molar_bear.jpg')))
+  profile = Profile.new(breed:"Mutt", location:"Chicago", age:8, size:"Medium", gender:"Male", spayed:true, image: random_image)
   dog.profile = profile
   profile.save
 end
